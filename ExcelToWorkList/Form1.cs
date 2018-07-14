c3f7416a3ea8966e39b51a5e188760554bcbf765
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2018-07-14 16:51:52 +0200

    A project to convert a Microsoft Excel to a Microsoft Word document

---
+++ ExcelToWorkList/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e) => openExcelFile.ShowDialog();
+
+        private void openExcelFile_FileOk(object sender, CancelEventArgs e)
+        {
+            tbInput.Text = openExcelFile.FileName;
+            btnGenerate.Enabled = true;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook wb = excel.Workbooks.Open(openExcelFile.FileName);
+
+            /*var excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            excelApp.Workbooks.Open(btnChoose2.Text);*/
+        }
+    }
+}
